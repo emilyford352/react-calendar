e76557c44a461748ed6fcee854120d537d62d305
emilyford352 <emilyford352@gmail.com>
2020-05-03 17:44:07 -0400

    Fixing day

--- calendar/src/Components/Day.js
+++ calendar/src/Components/Day.js
@@ -7,31 +7,9 @@ import { faMapMarker } from '@fortawesome/free-solid-svg-icons'
 
 const Show = (props) => {
     return (
-        <Grid container spacing={ 2 } alignItems="center" justify="center" className="show">
-        <Grid item xs={4}>
-        <h2>
-        { props.title }
-        &nbsp;
-    { props.link && <ShowLink link={ props.link }/> }
-    </h2>
-    </Grid>
-    <Grid item xs={1}>
-        </Grid>
-        <Grid item xs={4}>
-        <Grid container direction="column">
-        <Grid item className="show-place">
-        <b>
-        <FontAwesomeIcon icon={ faMapMarker } style={{color: '#b2d6da'}}/>
-    &nbsp;
-        { props.place }
-    </b>
-    </Grid>
-    <Grid item>
-    { props.date } - { props.time }
-    </Grid>
-    </Grid>
-    </Grid>
-    </Grid>
+        <div>
+            Create day in here...
+        </div>
     );
 }
 
