e58c22a584633b16757b540008f5e3d6d7cbf469
Furat Aziz <Furat@Furats-MBP-2.home>
2020-05-03 17:54:00 -0400

    Added dates for first 3 days of the month

--- calendar/src/Components/Calendar.js
+++ calendar/src/Components/Calendar.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Day from './Day';
 import '../App.css';
 
 const Calendar = () => {
     return (
         <div>
-            Calendar
+            <Day date="1st" />
+            <Day date="2nd" />
+            <Day date="3rd" />
         </div>
     );
 }
--- calendar/src/Components/Day.js
+++ calendar/src/Components/Day.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import '../App.css';
-import ShowLink from './ShowLink.js';
-import Grid from '@material-ui/core/Grid';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMapMarker } from '@fortawesome/free-solid-svg-icons'
 
 const Day = (props) => {
     return (
         <div>
-            Create day in here...
+            <div>
+            	<h3> {props.date} </h3>
+            </div>
         </div>
     );
 }
